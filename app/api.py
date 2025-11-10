8c017eb1f8915d376896f55c5d2b49f4f43e40d8
Hypotermya <migueljaimes10a@gmail.com>
2025-11-09 22:35:10 -0500

    Add CI/CD pipeline with GitHub Actions

--- app/api.py
+++ app/api.py
@@ -1,16 +1,18 @@
-# app/api.py
 from fastapi import FastAPI
 import joblib
 import pandas as pd
 from app.schemas import CustomerData
 
 app = FastAPI(title="Telco Churn Predictor", version="1.0")
+
+# Cargar modelo entrenado
 model = joblib.load("app/model.joblib")
 
 
 @app.get("/")
 def home():
-    return {"message": "API de predicción de churn de clientes de Telco está activa "}
+    msg = "API de predicción de churn de clientes de Telco está activa"
+    return {"message": msg}
 
 
 @app.post("/predict")
@@ -20,12 +22,13 @@ def predict(data: CustomerData):
 
     # Realizar predicción
     pred = model.predict(input_df)[0]
-    proba = model.predict_proba(input_df)[0][1]  # Probabilidad de churn (clase 1)
+    # Probabilidad de churn (clase 1)
+    proba = model.predict_proba(input_df)[0][1]
 
     # Interpretar la clase
     pred_label = "Churn" if pred == 1 else "No Churn"
 
     return {
         "prediction": pred_label,
-        "probability": round(float(proba), 3)
+        "probability": round(float(proba), 3),
     }
